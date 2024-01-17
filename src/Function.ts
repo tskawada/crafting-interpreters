4576f25f57efe2e629d7e0618082a8bfb2cb8d58
tskawada <yuta_kawada@outlook.jp>
2024-01-17 12:47:44 +0900

    remove unused module

--- src/Function.ts
+++ src/Function.ts
@@ -2,7 +2,6 @@ import { Callable } from "./Callable";
 import { Environment } from "./Environment";
 import { Interpreter } from "./Interpreter";
 import { Stmt } from "./Expr";
-import { Return } from "./Return";
 
 export class Function implements Callable {
     constructor(private declaration: InstanceType<typeof Stmt.Function>) {}
