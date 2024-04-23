6fef61c6cb6598671111b1c2fb431cf47ba4e8e1
tskawada <yuta_kawada@outlook.jp>
2024-04-23 11:51:57 +0900

    free the chunk

--- clox/memory.h
+++ clox/memory.h
@@ -9,6 +9,9 @@
 #define GROW_ARRAY(previous, type, oldCount, newCount) \
     (type*)reallocate(previous, sizeof(type) * (oldCount), sizeof(type) * (newCount))
 
+#define FREE_ARRAY(type, pointer, oldCount) \
+    reallocate(pointer, sizeof(type) * (oldCount), 0)
+
 void* reallocate(void* previous, size_t oldSize, size_t newSize);
 
 #endif
